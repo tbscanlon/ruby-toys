b37323a114c6d6f59f9a4d3408ff6c8627d11d03
tbscanlon <thomas.scanlon@outlook.com>
2016-06-27 20:19:12 +0100

    Create a password generator

---
+++ password-generator.rb
@@ -0,0 +1,22 @@
+number_of_passwords, length_of_passwords = ARGV
+instructions = "USAGE: password-generator.rb <number of passwords> <length of passwords>"
+
+$seed = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2",
+  "3", "4", "5", "6", "7", "8", "9", "0", "!", "\"", "£", "$", "%", "^",
+  "&", "*", "-", "_", "+", "="]
+
+def generate_password(number, length)
+  number.times do
+    password = []
+
+    length.times do
+      letter = $seed.sample
+      password.push(letter)
+    end
+      puts password.join("")
+  end
+end
+
+ARGV.first == nil ? (puts instructions) : generate_password(number_of_passwords.to_i, length_of_passwords.to_i)
